84d1269ffa891446adcf410592422bfbb8c531ad
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-09-22 23:16:51 +0200

    feat: add `is-schema` function in common package

--- packages/common/src/index.utils.ts
+++ packages/common/src/index.utils.ts
@@ -1,2 +1,3 @@
 export * from "./utils/get-reply-schema-with-error.js";
 export * from "./utils/is-error-schema.js";
+export * from "./utils/is-schema.js";
--- packages/common/src/utils/is-error-schema.ts
+++ packages/common/src/utils/is-error-schema.ts
@@ -1,6 +1,6 @@
-import { Value } from "@sinclair/typebox/value";
 import { ErrorSchema, type Error } from "../schemas/error-schema.js";
+import { isSchema } from "./is-schema.js";
 
 export function isErrorSchema(schema: unknown): schema is Error {
-  return Value.Check(ErrorSchema, schema);
+  return isSchema(ErrorSchema, schema);
 }

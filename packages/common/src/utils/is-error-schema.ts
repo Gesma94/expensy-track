11a1905b1d667d1f596bab54b93e02405e1a2191
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-03 00:50:10 +0200

    refactor(common): rename isErrorSchema

--- packages/common/src/utils/is-error-schema.ts
+++ packages/common/src/utils/is-error-schema.ts
@@ -1,6 +1,6 @@
 import { type RestError, RestErrorSchema } from '../schemas/error-schema.js';
 import { isSchema } from './is-schema.js';
 
-export function isErrorSchema(schema: unknown): schema is RestError {
+export function isRestErrorSchema(schema: unknown): schema is RestError {
   return isSchema(RestErrorSchema, schema);
 }

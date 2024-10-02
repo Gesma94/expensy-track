ecd5cb6a37e8a261ad554a3bc4ccf34ec9dc31e5
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-03 01:01:06 +0200

    refactor(common): change name of error-schema file

--- packages/common/src/index.schemas.ts
+++ packages/common/src/index.schemas.ts
@@ -1,3 +1,3 @@
-export * from './schemas/error-schema.js';
+export * from './schemas/rest-error-schema.js';
 export * from './schemas/user-schema.js';
 export * from './schemas/reply-authenticate-schema.js';
--- packages/common/src/utils/is-error-schema.ts
+++ packages/common/src/utils/is-error-schema.ts
@@ -1,4 +1,4 @@
-import { type RestError, RestErrorSchema } from '../schemas/error-schema.js';
+import { type RestError, RestErrorSchema } from '../schemas/rest-error-schema.js';
 import { isSchema } from './is-schema.js';
 
 export function isRestErrorSchema(schema: unknown): schema is RestError {

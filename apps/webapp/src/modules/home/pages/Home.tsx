1ce3692fa50990d9dcc23b353ded62f0295a3b75
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-04 01:19:04 +0200

    feat(webapp): add welcome message in Home

--- apps/webapp/src/modules/home/pages/Home.tsx
+++ apps/webapp/src/modules/home/pages/Home.tsx
@@ -1,7 +1,16 @@
+import { Link } from '@components/Link/Link';
+import { useAuth } from '@modules/auth/hooks/useAuth';
+
 export function Home() {
+  const { isAuthenticated, user } = useAuth();
+
   return (
     <>
       <h1>Expensy Track</h1>
+      <Link href='/login'>Login</Link>
+      <Link href='https://google.com'>outside</Link>
+      {isAuthenticated && <p>Welcome back, {user?.firstName}</p>}
+      {!isAuthenticated && <Link href='/login'>Login</Link>}
     </>
   );
 }

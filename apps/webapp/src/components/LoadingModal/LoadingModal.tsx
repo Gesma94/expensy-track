85669776a829e75f8000c93bbd64f3cbfdcafe82
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-04 01:15:47 +0200

    feat(webapp): add `LoadingModal` component

---
+++ apps/webapp/src/components/LoadingModal/LoadingModal.tsx
@@ -0,0 +1,38 @@
+import { Heading } from '@components/Heading/Heading';
+import clsx from 'clsx';
+import { Dialog, Modal, ModalOverlay, ProgressBar } from 'react-aria-components';
+import { useTranslation } from 'react-i18next';
+import { ClipLoader } from 'react-spinners';
+
+type Props = {
+  message?: string;
+  isTransparent?: boolean;
+};
+
+export const LoadingModal = ({ message, isTransparent }: Props) => {
+  const { t } = useTranslation('components', { keyPrefix: 'loading-modal' });
+
+  return (
+    <>
+      <ModalOverlay
+        className={clsx('fixed inset-0 bg-white grid place-items-center', isTransparent && 'bg-opacity-50')}
+        isDismissable={false}
+        isOpen={true}
+      >
+        <Modal isOpen={true} isDismissable={false}>
+          <Dialog className='flex text-center'>
+            <ProgressBar isIndeterminate={true} aria-label='loading progress bar'>
+              <ClipLoader size={16} speedMultiplier={0.68} />
+              <Heading level={4} slot='title' className='text-xl'>
+                {message ?? t('default-message')}
+                <span className='ml-[2px] animate-[pulse_1s_infinite_100ms]'>.</span>
+                <span className='animate-[pulse_1s_infinite_300ms]'>.</span>
+                <span className='animate-[pulse_1s_infinite_500ms]'>.</span>
+              </Heading>
+            </ProgressBar>
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </>
+  );
+};

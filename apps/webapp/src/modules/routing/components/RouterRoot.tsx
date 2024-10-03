d0b3840d17cd5e758ff52d8588114902b1587398
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-04 01:18:42 +0200

    feat(webapp): add missing RouterRoot component

---
+++ apps/webapp/src/modules/routing/components/RouterRoot.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider as AriaRouterProvider } from 'react-aria-components';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useAbsoluteHref } from '../hooks/useAbsoluteHref';
+
+export function RouterRoot() {
+  const navigate = useNavigate();
+
+  return (
+    <AriaRouterProvider navigate={navigate} useHref={useAbsoluteHref}>
+      <Outlet />
+    </AriaRouterProvider>
+  );
+}

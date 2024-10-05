b9d9d870e02e32a7036ab130b4c99f1a3ccdf810
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-05 17:20:20 +0200

    feat(common): adjust error code enum

--- packages/common/src/enums/error-code.ts
+++ packages/common/src/enums/error-code.ts
@@ -1,11 +1,9 @@
 export enum ErrorCode {
-  FST_JwyAuthorizationTokenExpired = 'FST_JWT_AUTHORIZATION_TOKEN_EXPIRED',
-  ET_InvalidAccessToken = 'ET_INVALID_ACCESS_TOKEN',
-  ET_InvalidRefreshToken = 'ET_INVALID_REFRESH_TOKEN',
   ET_InvalidGoogleIdToken = 'ET_INVALID_GOOGLE_ID_TOKEN',
   ET_EmailAlreadyUsed = 'ET_EMAIL_ALREADY_USED',
   ET_DisplayNameAlreadyUsed = 'ET_DISPLAY_NAME_ALREADY_USED',
   ET_InvalidCredentials = 'ET_INVALID_CREDENTIALS',
+  ET_UserUnauthorized = 'ET_USER_UNAUTHORIZED',
   ET_UserRegistrationFailed = 'ET_USER_REGISTRATION_FAILED',
-  ET_MockError = 'ET_MOCK_ERROR'
+  ET_UserNotExist = 'ET_USER_NOT_EXIST'
 }

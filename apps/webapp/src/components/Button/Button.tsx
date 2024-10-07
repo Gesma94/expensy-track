672fc6a70e60029a1c2803d65af21a00886c3659
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-08 01:48:04 +0200

    feat(webapp): add temporary border to Button

--- apps/webapp/src/components/Button/Button.tsx
+++ apps/webapp/src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { Button as AriaButton, type ButtonProps } from 'react-aria-components';
 
 export function Button(props: ButtonProps) {
-  return <AriaButton {...props} />;
+  return <AriaButton {...props} className='border border-black' />;
 }

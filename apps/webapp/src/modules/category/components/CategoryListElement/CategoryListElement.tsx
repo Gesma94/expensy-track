55bc508b184f7e5a3da434583edd7254d417406b
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-08 01:48:53 +0200

    feat(webapp): add confirmation dialog when deleting category

--- apps/webapp/src/modules/category/components/CategoryListElement/CategoryListElement.tsx
+++ apps/webapp/src/modules/category/components/CategoryListElement/CategoryListElement.tsx
@@ -1,7 +1,9 @@
 import { useMutation } from '@apollo/client';
 import { Button } from '@components/Button/Button';
 import { CategoryIcon } from '@components/CategoryIcon/CategoryIcon';
+import { ConfirmDialog } from '@components/dialogs/ConfirmDialog/ConfirmDialog';
 import { DELETE_CATEGORY } from '@modules/category/graphql/mutations';
+import { useState } from 'react';
 import { PiTrash } from 'react-icons/pi';
 import { GetMyCategoriesDocument, type MyCategoryFragment } from '../../../../gql/graphql';
 
@@ -10,23 +12,43 @@ type Props = {
 };
 
 export const CategoryListElement = ({ category }: Props) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const { icon, id, displayName } = category;
   const [deleteCategoryMutation, { error }] = useMutation(DELETE_CATEGORY, {
     awaitRefetchQueries: true,
     refetchQueries: [{ query: GetMyCategoriesDocument }]
   });
 
-  async function handleDeletePress() {
-    deleteCategoryMutation({ variables: { input: { id } } });
+  function handleDeletePress() {
+    setIsDialogOpen(true);
+  }
+
+  function handleCancel() {
+    setIsDialogOpen(false);
+  }
+
+  async function handleConfirm() {
+    await deleteCategoryMutation({ variables: { input: { id } } });
+    setIsDialogOpen(false);
   }
 
   return (
-    <div className='flex flex-row'>
-      <CategoryIcon icon={icon} />
-      <p>{displayName}</p>
-      <Button onPress={handleDeletePress}>
-        <PiTrash />
-      </Button>
-    </div>
+    <>
+      <ConfirmDialog
+        isOpen={isDialogOpen}
+        confirmLabel='Delete'
+        heading='Delete category'
+        message={`Are you sure you want to delete '${displayName}' category`}
+        onCancel={handleCancel}
+        onConfirm={handleConfirm}
+      />
+      <div className='flex flex-row'>
+        <CategoryIcon icon={icon} />
+        <p>{displayName}</p>
+        <Button onPress={handleDeletePress}>
+          <PiTrash />
+        </Button>
+      </div>
+    </>
   );
 };

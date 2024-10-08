e825dadd176bbe9c90b677a16841d8e59d7741fc
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-09 00:01:06 +0200

    feat(bff): support delete multiple categories

--- apps/bff/src/modules/category/graphql/mutations/delete-categories.ts
+++ apps/bff/src/modules/category/graphql/mutations/delete-categories.ts
@@ -3,22 +3,21 @@ import { getGqlUnsuccessResponse } from '#utils/get-gql-unsuccess-response.js';
 import { GraphqlErrorCode, type MutationResolvers } from '../../../../@types/graphql-generated.js';
 import { getGqlSuccessResponse } from '../../../../common/utils/get-gql-success-response.js';
 import { getGqlUnauthorizedResponse } from '../../../../common/utils/get-gql-unauthorized-response.js';
-import { CategoryToGraphql } from '../mappers/category.js';
 
-export const mutationDeleteCategory: MutationResolvers<MercuriusContext>['deleteCategory'] = async (
+export const mutationDeleteCategories: MutationResolvers<MercuriusContext>['deleteCategories'] = async (
   _parent,
   args,
   contextValue
 ) => {
-  const { id } = args.input;
+  const { ids } = args.input;
 
   if (!contextValue.user) {
     return getGqlUnauthorizedResponse();
   }
 
-  const category = await contextValue.app.prisma.category.delete({ where: { id } });
+  const categories = await contextValue.app.prisma.category.deleteMany({ where: { id: { in: ids } } });
 
-  return category
-    ? getGqlSuccessResponse(CategoryToGraphql(category))
-    : getGqlUnsuccessResponse(GraphqlErrorCode.CategoryIdNotExist, 'category does not exist');
+  return categories
+    ? getGqlSuccessResponse(categories.count)
+    : getGqlUnsuccessResponse(GraphqlErrorCode.DeleteManyFailed, 'could not delete categories from database');
 };

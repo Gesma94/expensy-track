f15b14821025412e9e3fcd79812952b24256f0e9
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-13 16:55:07 +0200

    fix(webapp): improve FormTextInput

--- apps/webapp/src/components/form/FormTextInput/FormTextInput.tsx
+++ apps/webapp/src/components/form/FormTextInput/FormTextInput.tsx
@@ -1,28 +1,23 @@
 import { TextInput, type TextInputProps } from '@components/input/TextInput/TextInput';
-import {
-  type Control,
-  Controller,
-  type ControllerProps,
-  type FieldPath,
-  type FieldValue,
-  type FieldValues,
-  type Path
-} from 'react-hook-form';
+import type { ComponentProps } from 'react';
+import { Controller, type ControllerProps, type FieldPath, type FieldValues } from 'react-hook-form';
 
 type Props<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
-> = Omit<ControllerProps<TFieldValues, TName>, 'render'> & TextInputProps;
+> = Omit<ControllerProps<TFieldValues, TName>, 'render'> & ComponentProps<typeof TextInput>;
 
 export function FormTextInput<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
->({ placeholder, label, ...controllerProps }: Props<TFieldValues, TName>) {
+>({ name, control, defaultValue, disabled, rules, shouldUnregister, ...textProps }: Props<TFieldValues, TName>) {
+  const controllerProps = { name, control, defaultValue, disabled, rules, shouldUnregister };
+
   return (
     <Controller
       {...controllerProps}
       render={({ field, fieldState: { invalid, error: _error } }) => (
-        <TextInput {...field} label={label} isInvalid={invalid} validationBehavior='aria' placeholder={placeholder} />
+        <TextInput {...textProps} {...field} isInvalid={invalid} validationBehavior='aria' />
       )}
     />
   );

be69d058d37dce9eee4787e94c1ae9870ad67ace
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-13 20:09:16 +0200

    feat(webapp): implement NumberInput components

---
+++ apps/webapp/src/components/form/FormNumberInput/FormNumberInput.tsx
@@ -0,0 +1,30 @@
+import { NumberInput } from '@components/input/NumberInput/NumberInput';
+import type { ComponentProps } from 'react';
+import { Controller, type ControllerProps, type FieldPath, type FieldValues } from 'react-hook-form';
+
+type Props<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = Omit<ControllerProps<TFieldValues, TName>, 'render'> & ComponentProps<typeof NumberInput>;
+
+export function FormNumberInput<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({ name, control, defaultValue, disabled, rules, shouldUnregister, ...numberProps }: Props<TFieldValues, TName>) {
+  const controllerProps = { name, control, defaultValue, disabled, rules, shouldUnregister };
+
+  return (
+    <Controller
+      {...controllerProps}
+      render={({ field: { disabled, ...fieldProps }, fieldState: { invalid, error: _error } }) => (
+        <NumberInput
+          {...numberProps}
+          {...fieldProps}
+          isDisabled={disabled}
+          isInvalid={invalid}
+          validationBehavior='aria'
+        />
+      )}
+    />
+  );
+}
---
+++ apps/webapp/src/components/input/NumberInput/NumberInput.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@components/Button/Button';
+import { forwardRef } from 'react';
+import {
+  Group as AriaGroup,
+  Input as AriaInput,
+  Label as AriaLabel,
+  NumberField as AriaNumberField,
+  type NumberFieldProps
+} from 'react-aria-components';
+
+export type NumberInputProps = {
+  label: string;
+};
+
+type Props = NumberFieldProps & React.RefAttributes<HTMLInputElement> & NumberInputProps;
+
+export const NumberInput = forwardRef<HTMLInputElement, Props>(function _TextInput({ label, ...props }, ref) {
+  return (
+    <AriaNumberField {...props}>
+      <AriaLabel>{label}</AriaLabel>
+      <AriaGroup>
+        <AriaInput ref={ref} />
+        <Button slot='decrement'>-</Button>
+        <Button slot='increment'>+</Button>
+      </AriaGroup>
+    </AriaNumberField>
+  );
+});

d94d7bf678a0923fca6bfe08520ada42fbab7c81
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-17 00:54:06 +0200

    refactor(webapp): rename files

--- apps/bff/src/modules/transaction/loaders.ts
+++ apps/bff/src/modules/transaction/loaders.ts
@@ -1,8 +1,8 @@
 import type { MercuriusLoaderTyped } from '#types/graphql-loaders.js';
 import { transactionCategoryLoader } from './loaders/category.js';
 import { transactionLabelsLoader } from './loaders/labels.js';
-import { transactionParentTransactionLoader } from './loaders/parentTransaction.js';
-import { transactionSubTransactionsLoader } from './loaders/subTransactions.js';
+import { transactionParentTransactionLoader } from './loaders/parent-transaction.js';
+import { transactionSubTransactionsLoader } from './loaders/sub-transactions.js';
 import { transactionWalletLoader } from './loaders/wallet.js';
 
 export const transactionLoader: MercuriusLoaderTyped = {

8f56abbbb60e3d17c0ef3713d6fb0331af75869e
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-22 23:50:05 +0200

    feat(bff): add gql resolvers param type

--- apps/bff/src/common/types/graphql-loaders.ts
+++ apps/bff/src/common/types/graphql-loaders.ts
@@ -13,7 +13,13 @@ type ExtractResolverReturnType<T> = T extends ResolverFn<infer TResult, infer _T
     ? NonNullable<TResult>
     : never;
 
-type LoaderQueriesArg<TObj extends keyof ResolversParentTypes, TParams extends {} = object> = Array<{
+type ExtractArgs<T> = T extends ResolverFn<infer _TResult, infer _TParent, infer _TContext, infer TArgs>
+  ? NonNullable<TArgs>
+  : T extends ResolverWithResolve<infer _TResult, infer _TParent, infer _TContext, infer TArgs>
+    ? NonNullable<TArgs>
+    : never;
+
+type LoaderQueriesArg<TObj extends keyof ResolversParentTypes, TParams> = Array<{
   obj: ResolversParentTypes[TObj];
   params: TParams;
 }>;
@@ -21,7 +27,7 @@ type LoaderQueriesArg<TObj extends keyof ResolversParentTypes, TParams extends {
 export type MercuriusLoaderTyped<TContext = MercuriusContext> = {
   [K in keyof Resolvers<TContext>]: {
     [Field in keyof Required<Resolvers<TContext>>[K]]: (
-      queries: LoaderQueriesArg<K>,
+      queries: LoaderQueriesArg<K, ExtractArgs<Required<Resolvers<TContext>>[K][Field]>>,
       context: TContext
     ) => Promise<Array<Maybe<ExtractResolverReturnType<Required<Resolvers<TContext>>[K][Field]>>>>;
   };

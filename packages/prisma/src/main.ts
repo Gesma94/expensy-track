c97dd3338c20bc73bf70ac7e88c5f8622ea75c9a
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-22 23:48:58 +0200

    feat(prisma): add utils types

--- packages/prisma/src/main.ts
+++ packages/prisma/src/main.ts
@@ -7,6 +7,7 @@ import type {
   GuestsOnWallets,
   Label,
   LabelsOnTransactions,
+  Prisma,
   Transaction,
   User,
   Wallet
@@ -25,3 +26,11 @@ export type {
   Wallet
 };
 export { PrismaClient, $Enums };
+
+export namespace $Utils {
+  export type Models = keyof typeof Prisma.ModelName;
+  export type ArgsType<T extends Models> = Prisma.TypeMap['model'][T]['operations']['findMany']['args'];
+  export type WhereType<T extends Models> = NonNullable<ArgsType<T>['where']>;
+  export type AndType<T extends Models> = NonNullable<WhereType<T>['AND']>;
+  export type OrType<T extends Models> = NonNullable<WhereType<T>['OR']>;
+}

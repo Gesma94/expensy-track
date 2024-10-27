f8d699a82fe43be263beb4f06485561ddecf7060
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-27 18:01:49 +0100

    feat(webapp): implement FormCheckbox

---
+++ apps/webapp/src/components/form/FormCheckbox/FormCheckbox.tsx
@@ -0,0 +1,23 @@
+import { Checkbox } from '@components/input/Checkbox/Checkbox';
+import type { ComponentProps } from 'react';
+import { Controller, type ControllerProps, type FieldPath, type FieldValues } from 'react-hook-form';
+
+type Props<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = Omit<ControllerProps<TFieldValues, TName>, 'render'> & ComponentProps<typeof Checkbox>;
+
+export function FormCheckbox<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({ name, control, defaultValue, disabled, rules, shouldUnregister, ...textProps }: Props<TFieldValues, TName>) {
+  const controllerProps = { name, control, defaultValue, disabled, rules, shouldUnregister };
+  return (
+    <Controller
+      {...controllerProps}
+      render={({ field: { disabled, ...fieldProps }, fieldState: { invalid, error: _error } }) => (
+        <Checkbox {...textProps} {...fieldProps} isDisabled={disabled} isInvalid={invalid} validationBehavior='aria' />
+      )}
+    />
+  );
+}

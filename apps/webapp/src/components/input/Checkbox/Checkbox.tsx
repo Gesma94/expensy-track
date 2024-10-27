70813977b47806df7d23c5fadbc2787c55068eb9
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-27 17:59:26 +0100

    feat(webapp): add ref prop to Checkbox

--- apps/webapp/src/components/input/Checkbox/Checkbox.tsx
+++ apps/webapp/src/components/input/Checkbox/Checkbox.tsx
@@ -1,9 +1,12 @@
 import clsx from 'clsx';
+import { type RefAttributes, forwardRef } from 'react';
 import { Checkbox as AriaCheckbox, type CheckboxProps } from 'react-aria-components';
 
-export function Checkbox({ children, ...otherProps }: CheckboxProps) {
+type Props = CheckboxProps & RefAttributes<HTMLLabelElement>;
+
+export const Checkbox = forwardRef<HTMLLabelElement, Props>(function _Checkbox({ children, ...otherProps }, ref) {
   return (
-    <AriaCheckbox {...otherProps}>
+    <AriaCheckbox {...otherProps} ref={ref}>
       {({ isSelected }) => (
         <>
           <div className={clsx('size-4 border-black border', isSelected ? 'bg-black' : 'bg-white')} />
@@ -12,4 +15,4 @@ export function Checkbox({ children, ...otherProps }: CheckboxProps) {
       )}
     </AriaCheckbox>
   );
-}
+});

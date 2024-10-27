ca0004479b3e0132a991c49d193247a3927c1ebd
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-10-28 00:10:56 +0100

    feat(webapp): add forwardRef to Button element

--- apps/webapp/src/components/Button/Button.tsx
+++ apps/webapp/src/components/Button/Button.tsx
@@ -1,10 +1,17 @@
+import { forwardRef } from 'react';
 import { Button as AriaButton, type ButtonProps, type ButtonRenderProps } from 'react-aria-components';
 import { twMerge } from 'tailwind-merge';
 
-export function Button({ className, ...otherProps }: ButtonProps) {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(function _Button({ className, ...otherProps }, ref) {
   function getAriaClassName(values: ButtonRenderProps & { defaultClassName: string | undefined }) {
     return typeof className === 'function' ? className(values) : className;
   }
 
-  return <AriaButton {...otherProps} className={values => twMerge('border border-black', getAriaClassName(values))} />;
-}
+  return (
+    <AriaButton
+      ref={ref}
+      {...otherProps}
+      className={values => twMerge('border border-black', getAriaClassName(values))}
+    />
+  );
+});

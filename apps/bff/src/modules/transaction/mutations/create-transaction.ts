37b4d2b88fa0b3cd33d7a81084f7019a9e923e69
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-01 21:05:04 +0100

    feat(bff): add create transaction mutation

---
+++ apps/bff/src/modules/transaction/mutations/create-transaction.ts
@@ -0,0 +1,57 @@
+import type { MercuriusContext } from 'mercurius';
+import { getGqlSuccessResponse } from '#utils/get-gql-success-response.js';
+import { getGqlUnauthorizedResponse } from '#utils/get-gql-unauthorized-response.js';
+import { getGqlUnsuccessResponse } from '#utils/get-gql-unsuccess-response.js';
+import { GraphqlErrorCode, type MutationResolvers } from '../../../@types/graphql-generated.js';
+import { TransactionToGraphql } from '../mappers/transaction.js';
+
+export const mutationCreateTransaction: MutationResolvers<MercuriusContext>['createTransaction'] = async (
+  _parent,
+  args,
+  contextValue
+) => {
+  const user = contextValue.user;
+  const { amount, categoryId, date, isParent, labelsIds, note, walletId } = args.input;
+
+  if (!user) {
+    return getGqlUnauthorizedResponse();
+  }
+
+  const wallet = await contextValue.app.prisma.wallet.findUnique({
+    where: {
+      id: walletId,
+      OR: [{ ownerId: user.id }, { guests: { some: { userId: user.id } } }]
+    }
+  });
+
+  if (!wallet) {
+    return getGqlUnsuccessResponse(GraphqlErrorCode.WalletNotGound, 'wallet with given id not found');
+  }
+
+  const result = await contextValue.app.prisma.$transaction(async tx => {
+    const transaction = await tx.transaction.create({
+      data: {
+        note,
+        date,
+        amount,
+        walletId,
+        isParent,
+        categoryId,
+        userId: user.id
+      }
+    });
+
+    if (labelsIds && labelsIds?.length > 0) {
+      await tx.labelsOnTransactions.createMany({
+        data: labelsIds.map(labelId => ({
+          labelId,
+          transactionId: transaction.id
+        }))
+      });
+    }
+
+    return transaction;
+  });
+
+  return getGqlSuccessResponse(TransactionToGraphql(result));
+};
--- apps/bff/src/modules/transaction/resolvers.ts
+++ apps/bff/src/modules/transaction/resolvers.ts
@@ -1,8 +1,12 @@
 import type { Resolvers } from '../../@types/graphql-generated.js';
+import { mutationCreateTransaction } from './mutations/create-transaction.js';
 import { queryTransactions } from './queries/transactions.js';
 
 export const transactionResolvers: Resolvers = {
   Query: {
     transactions: queryTransactions
+  },
+  Mutation: {
+    createTransaction: mutationCreateTransaction
   }
 };

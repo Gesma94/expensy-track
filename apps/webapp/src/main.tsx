49da164dcdc251c3441ec9f8a2a65949ce2797ef
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-04 20:50:30 +0100

    fix(webapp): remove apollo/client dependency

--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './modules/auth/components/AuthContext/AuthContext';
 import { ReactQueryProvider } from './modules/fetch/components/ReactQueryProvider/ReactQueryProvider';
 import './i18n/config';
-import { ReactApolloProvider } from './modules/fetch/components/ReactApolloProvider/ReactApolloProvider';
 import './index.css';
 import { getRouter } from '@modules/routing/utils/getRouter';
 import { ToastProvider } from '@modules/toast/components/ToastProvider/ToastProvider';
@@ -14,13 +13,11 @@ const router = getRouter();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ToastProvider>
-      <ReactApolloProvider>
-        <ReactQueryProvider>
-          <AuthProvider>
-            <RouterProvider router={router} />
-          </AuthProvider>
-        </ReactQueryProvider>
-      </ReactApolloProvider>
+      <ReactQueryProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </ReactQueryProvider>
     </ToastProvider>
   </StrictMode>
 );

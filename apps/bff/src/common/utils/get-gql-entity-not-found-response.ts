633efcce45b0bc1669ef60625568782488b2329e
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-14 00:06:31 +0100

    feat(bff): add getGqlEntityNotFoundResponse

---
+++ apps/bff/src/common/utils/get-gql-entity-not-found-response.ts
@@ -0,0 +1,7 @@
+import { GraphqlErrorCode } from '../../@types/graphql-generated.js';
+import type { GqlResponseType } from '../types/gql-response-type.js';
+import { getGqlUnsuccessResponse } from './get-gql-unsuccess-response.js';
+
+export function getGqlEntityNotFoundResponse(message: string): GqlResponseType<null> {
+  return getGqlUnsuccessResponse(GraphqlErrorCode.EntityNotFound, message, null);
+}

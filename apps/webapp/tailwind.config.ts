9ce564d3db7a48aee5253969dbd82f88ca9f462e
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-14 01:28:06 +0100

    feat(webapp): add fonts and set default font for body

--- apps/webapp/tailwind.config.ts
+++ apps/webapp/tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        bitter: ['Bitter', 'serif'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
   },
   plugins: []
 } satisfies Config;

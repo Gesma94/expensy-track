a1fa15667c64ba56f35cf2d6a87af8ebb923e66e
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-16 19:24:02 +0100

    feat(webapp): add input height in tailwind theme

--- apps/webapp/tailwind.config.ts
+++ apps/webapp/tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      height: {
+        input: '2.5rem'
+      },
       fontFamily: {
         bitter: ['Bitter', 'serif'],
         poppins: ['Poppins', 'sans-serif']

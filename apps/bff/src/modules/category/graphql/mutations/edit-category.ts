4089bb9346e84c4bad46ee03e43c012ba46f1716
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-18 01:16:12 +0100

    feat(bff): add edit category mutation

---
+++ apps/bff/src/modules/category/graphql/mutations/edit-category.ts
@@ -0,0 +1,34 @@
+import type { MercuriusContext } from 'mercurius';
+import { getGqlUnsuccessResponse } from '#utils/get-gql-unsuccess-response.js';
+import { GraphqlErrorCode, type MutationResolvers } from '../../../../@types/graphql-generated.js';
+import { getGqlSuccessResponse } from '../../../../common/utils/get-gql-success-response.js';
+import { getGqlUnauthorizedResponse } from '../../../../common/utils/get-gql-unauthorized-response.js';
+import { CategoryIconToPrisma } from '../mappers/category-icon.js';
+import { CategoryToGraphql } from '../mappers/category.js';
+
+export const mutationEditCategory: MutationResolvers<MercuriusContext>['editCategory'] = async (
+  _parent,
+  args,
+  contextValue
+) => {
+  const { color, displayName, icon, id } = args.input;
+  const prismaCategoryIcon = CategoryIconToPrisma(icon);
+
+  if (!contextValue.user) {
+    return getGqlUnauthorizedResponse();
+  }
+
+  try {
+    const updatedCategory = await contextValue.app.prisma.category.update({
+      data: { color, displayName, icon: prismaCategoryIcon },
+      where: { id, userId: contextValue.user.id }
+    });
+
+    return getGqlSuccessResponse(CategoryToGraphql(updatedCategory));
+  } catch (error) {
+    const errorMessage = `could not update category with id ${id} from user ${contextValue.user.id}`;
+
+    contextValue.app.log.error(error, errorMessage);
+    return getGqlUnsuccessResponse(GraphqlErrorCode.OperationFailed, errorMessage);
+  }
+};
--- apps/bff/src/modules/category/graphql/resolvers.ts
+++ apps/bff/src/modules/category/graphql/resolvers.ts
@@ -1,6 +1,7 @@
 import type { Resolvers } from '../../../@types/graphql-generated.js';
 import { mutationCreateCategory } from './mutations/create-category.js';
 import { mutationDeleteCategories } from './mutations/delete-categories.js';
+import { mutationEditCategory } from './mutations/edit-category.js';
 import { queryCategories } from './queries/categories.js';
 
 export const categoryResolvers: Resolvers = {
@@ -8,6 +9,7 @@ export const categoryResolvers: Resolvers = {
     categories: queryCategories
   },
   Mutation: {
+    editCategory: mutationEditCategory,
     createCategory: mutationCreateCategory,
     deleteCategories: mutationDeleteCategories
   }

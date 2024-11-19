8279c2df09ca0e7fd1c20ebd8f3b918f0afc6b32
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-19 22:54:04 +0100

    fix(bff): load routes manually due to incompatibility with vitest

--- apps/bff/src/app.ts
+++ apps/bff/src/app.ts
@@ -6,6 +6,12 @@ import Fastify from 'fastify';
 import { minimatch } from 'minimatch';
 import type { FastifyBuildOptions } from './common/types/fastify-build-options.js';
 import { getFastifyLogger } from './common/utils/get-fastify-logger.js';
+import authenticateRoute from './modules/auth/routes/authenticate.js';
+import loginGoogleRoute from './modules/auth/routes/login-google.js';
+import loginRoute from './modules/auth/routes/login.js';
+import logoutRoute from './modules/auth/routes/logout.js';
+import refreshTokenRoute from './modules/auth/routes/refresh-token.js';
+import signUpRoute from './modules/auth/routes/sign-up.js';
 
 export async function buildFastify(options?: FastifyBuildOptions) {
   const __filename = fileURLToPath(import.meta.url);
@@ -23,14 +29,22 @@ export async function buildFastify(options?: FastifyBuildOptions) {
   });
 
   // registering all routes in 'routes' folder with prefix "/api"
-  fastify.register(autoLoad, {
-    dir: join(__dirname, 'modules'),
-    dirNameRoutePrefix: false,
-    matchFilter: path => minimatch(path, '**/routes/*.ts'),
-    options: {
-      prefix: '/api'
-    }
-  });
+  fastify.register(authenticateRoute, { prefix: '/api' });
+  fastify.register(loginGoogleRoute, { prefix: '/api' });
+  fastify.register(loginRoute, { prefix: '/api' });
+  fastify.register(logoutRoute, { prefix: '/api' });
+  fastify.register(refreshTokenRoute, { prefix: '/api' });
+  fastify.register(signUpRoute, { prefix: '/api' });
+
+  // !WORKAROUND: autoLoad is not working with vitest if the provided folder contains any file that is not a registrable plugin
+  // fastify.register(autoLoad, {
+  //   dir: join(__dirname, 'modules'),
+  //   dirNameRoutePrefix: false,
+  //   matchFilter: path => minimatch(path, '**/routes/*.ts'),
+  //   options: {
+  //     prefix: '/api'
+  //   }
+  // });
 
   fastify.get('/ping', (_, reply) => {
     reply.send({ ping: 'pong' });

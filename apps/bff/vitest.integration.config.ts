5bfab7bba0b5563c6bf5aa8201e2c9b77a11fd6e
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-11-19 22:53:31 +0100

    chore(bff): move vitest config in root folder

---
+++ apps/bff/vitest.integration.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    hookTimeout: 10000,
+    setupFiles: './vitest.integration.setup.ts',
+    include: ['./test/**/*.test.ts'],
+    alias: {
+      '#enums': new URL('./src/common/enums', import.meta.url).pathname,
+      '#schemas': new URL('./src/common/schemas', import.meta.url).pathname,
+      '#types': new URL('./src/common/types', import.meta.url).pathname,
+      '#utils': new URL('./src/common/utils', import.meta.url).pathname
+    },
+    server: {
+      deps: {
+        inline: ['@fastify/autoload']
+      }
+    }
+  }
+});

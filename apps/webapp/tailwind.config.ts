da27eaf61c3fb48ecd9bbc2ee1ad39de5f89bcb7
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-06 01:24:48 +0100

    feat(webapp): change fonts and add colors in tailwind theme

--- apps/webapp/tailwind.config.ts
+++ apps/webapp/tailwind.config.ts
@@ -8,8 +8,19 @@ export default {
         input: '2.5rem'
       },
       fontFamily: {
-        bitter: ['Bitter', 'serif'],
-        poppins: ['Poppins', 'sans-serif']
+        inter: ['Inter', 'sans-serif'],
+        sora: ['Sora', 'sans-serif']
+      },
+      colors: {
+        white: '#FFFFFF',
+        cultured: '#F4F4F8',
+        'metallic-silver': '#A8A9B2',
+        'american-silver': '#CACBD4',
+        'eerie-black': '#17171D',
+        independence: '#565668',
+        'blue-violet': '#8A28F4',
+        'celtic-blue': '#145DDA',
+        'alice-blue': '#E9F1FF'
       }
     }
   },

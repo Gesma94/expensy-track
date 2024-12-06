aecd096a84dba293bed8a4553a2b98aeeae44986
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-06 01:26:56 +0100

    feat(bff): set fastify logger with pino-pretty for e2e node env

--- apps/bff/src/common/utils/get-fastify-logger.ts
+++ apps/bff/src/common/utils/get-fastify-logger.ts
@@ -7,6 +7,7 @@ export function getFastifyLogger(nodeEnv: string | undefined): PinoLoggerOptions
   switch (nodeEnv) {
     case 'test':
     case 'development':
+    case 'e2e':
       return {
         level,
         enabled: true,
@@ -14,7 +15,6 @@ export function getFastifyLogger(nodeEnv: string | undefined): PinoLoggerOptions
           target: 'pino-pretty'
         }
       };
-    case 'e2e':
     case 'production':
       return {
         level,

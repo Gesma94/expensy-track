a1e104a2afaae404649efc69d9b1963f8c64b6a1
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-10 00:39:50 +0100

    feat(webapp): improve style of checkbox

--- apps/webapp/src/components/ui/input/Checkbox/Checkbox.tsx
+++ apps/webapp/src/components/ui/input/Checkbox/Checkbox.tsx
@@ -1,8 +1,36 @@
 import { getAriaCustomClassName } from '@common/utils/get-aria-custom-class-name';
-import clsx from 'clsx';
 import { type RefAttributes, forwardRef } from 'react';
 import { Checkbox as AriaCheckbox, type CheckboxProps } from 'react-aria-components';
+import { PiCheck } from 'react-icons/pi';
 import { twMerge } from 'tailwind-merge';
+import { tv } from 'tailwind-variants';
+
+const checkboxStyle = tv({
+  slots: {
+    checkboxDiv: 'size-6 rounded transition-colors duration-500 flex items-center justify-center',
+    checkIcon: 'fill-white size-4 transition-all duration-500'
+  },
+  variants: {
+    isSelected: {
+      true: {
+        checkboxDiv: 'bg-celtic-blue border-none',
+        checkIcon: 'visible opacity-100 scale-100 rotate-[360deg]'
+      },
+      false: {
+        checkboxDiv: 'bg-white border border-american-silver',
+        checkIcon: 'opacity-0 invisible scale-0 rotate-180'
+      }
+    },
+    isFocused: {
+      true: {
+        checkboxDiv: ''
+      },
+      false: {
+        checkboxDiv: ''
+      }
+    }
+  }
+});
 
 type Props = CheckboxProps & RefAttributes<HTMLLabelElement>;
 
@@ -18,12 +46,18 @@ export const Checkbox = forwardRef<HTMLLabelElement, Props>(function _Checkbox(
       }
       ref={ref}
     >
-      {({ isSelected, isFocused }) => (
-        <>
-          <div className={clsx('size-4 border-black border', isSelected ? 'bg-black' : 'bg-white', isFocused && '')} />
-          {children}
-        </>
-      )}
+      {({ isSelected, isFocused }) => {
+        const { checkboxDiv, checkIcon } = checkboxStyle({ isSelected, isFocused });
+
+        return (
+          <>
+            <div className={checkboxDiv()}>
+              <PiCheck className={checkIcon()} aria-hidden={!isSelected} />
+            </div>
+            {children}
+          </>
+        );
+      }}
     </AriaCheckbox>
   );
 });

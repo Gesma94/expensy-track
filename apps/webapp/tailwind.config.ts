c22ea35b0b18d781f9870f0afc593a1a70ec471d
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-10 01:05:57 +0100

    feat(webapp): set colors to tailwind theme

--- apps/webapp/tailwind.config.ts
+++ apps/webapp/tailwind.config.ts
@@ -3,6 +3,19 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#FFFFFF',
+      cultured: '#F4F4F8',
+      'metallic-silver': '#A8A9B2',
+      'american-silver': '#CACBD4',
+      'eerie-black': '#17171D',
+      independence: '#565668',
+      'blue-violet': '#8A28F4',
+      'celtic-blue': '#145DDA',
+      'alice-blue': '#E9F1FF',
+      'ghost-white': '#F7FBFF',
+      'lavender-blue': '#B8D2FF'
+    },
     extend: {
       height: {
         input: '2.5rem'
@@ -10,17 +23,6 @@ export default {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
         sora: ['Sora', 'sans-serif']
-      },
-      colors: {
-        white: '#FFFFFF',
-        cultured: '#F4F4F8',
-        'metallic-silver': '#A8A9B2',
-        'american-silver': '#CACBD4',
-        'eerie-black': '#17171D',
-        independence: '#565668',
-        'blue-violet': '#8A28F4',
-        'celtic-blue': '#145DDA',
-        'alice-blue': '#E9F1FF'
       }
     }
   },

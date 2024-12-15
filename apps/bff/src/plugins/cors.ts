738d4518f973e256e069dcf0568b8565a2f28c2e
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-15 21:54:58 +0100

    chore(bff): add log while registering cors plugin

--- apps/bff/src/plugins/cors.ts
+++ apps/bff/src/plugins/cors.ts
@@ -4,6 +4,8 @@ import { FastifyPluginName } from '../common/enums/fastify-plugin-name.js';
 
 export default fp(
   (fastify, _, done) => {
+    fastify.log.info(`Registering CORS origin: "${fastify.env.CORS_ORIGIN}"`);
+
     fastify.register(cors, {
       origin: fastify.env.CORS_ORIGIN,
       credentials: true

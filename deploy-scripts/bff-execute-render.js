c1a0c8bd21e45988f38c9a3c82fcda6478999780
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-15 20:31:38 +0100

    ci(bff): add script for executing bff on render

---
+++ deploy-scripts/bff-execute-render.js
@@ -0,0 +1,32 @@
+import { execSync } from 'node:child_process';
+import { join, resolve } from 'node:path';
+
+const runCommand = (command, cwd) => {
+  console.log(`Running: ${command} in ${cwd || 'current directory'}`);
+  execSync(command, { stdio: 'inherit', cwd });
+};
+
+try {
+  // Adjust the paths relative to the new location of deploy-bff.js
+  const rootDir = resolve(__dirname, '../'); // Move up to the monorepo root
+
+  console.log('Installing all dependencies...');
+  runCommand('pnpm ci', rootDir);
+
+  console.log('Building common package...');
+  runCommand('pnpm build', join(rootDir, 'packages/common'));
+
+  console.log('Building prisma package...');
+  runCommand('pnpm build', join(rootDir, 'packages/prisma'));
+
+  console.log('Generating BFF graphql types...');
+  runCommand('pnpm graphql-codegen', join(rootDir, 'apps/bff'));
+
+  console.log('Building BFF...');
+  runCommand('pnpm build', join(rootDir, 'apps/bff'));
+
+  console.log('Deployment complete!');
+} catch (error) {
+  console.error('Error during deployment:', error.message);
+  process.exit(1);
+}

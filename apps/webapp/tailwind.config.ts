73884ae584181fe7bdf0d9903e9a41130f4dde3d
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-20 21:19:09 +0100

    feat(webapp): improve tailwind theme color

--- apps/webapp/tailwind.config.ts
+++ apps/webapp/tailwind.config.ts
@@ -17,7 +17,29 @@ export default {
       'alice-blue': '#E9F1FF',
       'ghost-white': '#F7FBFF',
       'lavender-blue': '#B8D2FF',
-      rhythm: '#77778F'
+      rhythm: '#77778F',
+
+      primary: {
+        DEFAULT: '#4CAF50', // Main primary color
+        hover: '#43A047', // Slightly darker shade for hover state
+        active: '#388E3C', // Even darker shade for active state
+        focus: '#81C784', // Lighter shade for focus outline
+        disabled: '#A5D6A7', // Muted shade for disabled state
+        text: '#FFFFFF' // Text color on primary background
+      },
+      'primary-light': '#81C784', // Lighter shade of primary
+      'primary-dark': '#388E3C', // Darker shade of primary
+      secondary: '#2196F3', // Blue, for links or secondary accents
+      'secondary-light': '#64B5F6',
+      'secondary-dark': '#1976D2',
+      success: '#4CAF50', // Used for success alerts or positive trends
+      warning: '#FF9800', // Used for warnings or alerts
+      error: '#F44336', // Used for errors or negative trends
+      'text-primary': '#212121', // Main text color
+      'text-secondary': '#757575', // Secondary text
+      'background-light': '#FAFAFA', // Light background for cards and UI
+      'background-dark': '#263238', // Darker background,
+      transparent: '#FFFFFF00'
     },
     extend: {
       padding: {

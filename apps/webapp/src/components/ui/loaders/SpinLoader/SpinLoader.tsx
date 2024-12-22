0fcbe4ab71a2c6305a8415f791fdcb39bc907a7e
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-22 15:41:09 +0100

    feat(webapp): improve `SpinLoader` component

--- apps/webapp/src/components/ui/loaders/SpinLoader/SpinLoader.tsx
+++ apps/webapp/src/components/ui/loaders/SpinLoader/SpinLoader.tsx
@@ -1,17 +1,22 @@
+import type { TailwindColors } from '@common/types/tailwind-colors';
 import { twMerge } from 'tailwind-merge';
+import { type VariantProps, tv } from 'tailwind-variants';
 
-type Props = {
-  className?: string;
+const spinLoaderStyle = tv({
+  base: 'inline-block rounded-[50%] border-solid box-border animate-spin',
+  variants: {
+    size: {
+      small: 'size-4 border-2',
+      default: 'size-6 border-4',
+      large: 'size-12 border-8'
+    }
+  }
+});
+
+type Props = VariantProps<typeof spinLoaderStyle> & {
+  color?: TailwindColors;
 };
 
-export function SpinLoader({ className }: Props) {
-  return (
-    <span
-      className={twMerge(
-        'block size-8 border-4 border-primary-text rounded-[50%] box-border animate-spin',
-        className,
-        'border-b-transparent'
-      )}
-    />
-  );
+export function SpinLoader({ color = 'primary', size = 'default' }: Props) {
+  return <span className={twMerge(spinLoaderStyle({ size }), `border-${color}`, 'border-b-transparent')} />;
 }

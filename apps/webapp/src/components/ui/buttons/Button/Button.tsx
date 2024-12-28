d80b203db12bb8b765e3574454cc18042d251d24
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-28 15:44:43 +0100

    feat(webapp): improve Button component

--- apps/webapp/src/components/ui/buttons/Button/Button.tsx
+++ apps/webapp/src/components/ui/buttons/Button/Button.tsx
@@ -1,85 +1,11 @@
+import type { IconType } from '@common/enums/icon';
 import { getAriaRenderChildren } from '@common/utils/get-aria-render-children';
+import { Icon } from '@components/ui/icon/Icon/Icon';
 import { SpinLoader } from '@components/ui/loaders/SpinLoader/SpinLoader';
 import { type ComponentProps, forwardRef } from 'react';
 import { Button as AriaButton, type ButtonRenderProps } from 'react-aria-components';
-import type { IconType } from 'react-icons/lib';
 import { twMerge } from 'tailwind-merge';
 import { type VariantProps, tv } from 'tailwind-variants';
-
-const buttonStyle = tv({
-  base: 'flex items-center font-medium justify-center transition-colors duration-300',
-  variants: {
-    size: {
-      small: 'gap-2 h-8 px-2 text-xs rounded',
-      default: 'gap-3 h-10 px-4 text-sm rounded-md',
-      large: 'gap-4 h-12 px-8 text-lg rounded-lg'
-    },
-    fullWidth: {
-      true: 'w-full',
-      false: ''
-    },
-    variant: {
-      ghost: '',
-      outline: 'bg-white text-eerie-black border border-american-silver',
-      primary: ''
-    },
-    isDisabled: {
-      true: 'bg-cultured text-metallic-silver',
-      false: ''
-    },
-    isLoading: {
-      true: 'relative',
-      false: ''
-    }
-  },
-  compoundVariants: [
-    {
-      variant: 'primary',
-      size: 'default',
-      className: 'font-semibold'
-    },
-    {
-      variant: 'primary',
-      size: 'large',
-      className: 'font-bold'
-    },
-    {
-      variant: 'primary',
-      isDisabled: true,
-      isLoading: false,
-      className: 'bg-primary-disabled text-primary-text'
-    },
-    {
-      variant: 'primary',
-      isDisabled: true,
-      isLoading: true,
-      className: 'bg-primary-disabled text-primary-text'
-    },
-    {
-      variant: 'primary',
-      isLoading: false,
-      isDisabled: false,
-      className:
-        'bg-primary text-primary-text hover:bg-primary-hover active:bg-primary-active focus:outline-offset-2 focus:outline-primary-focus'
-    },
-    {
-      variant: 'primary',
-      isLoading: true,
-      isDisabled: false,
-      className: 'bg-primary cursor-auto'
-    }
-  ]
-});
-
-const childrenSpanStyle = tv({
-  base: '',
-  variants: {
-    isLoading: {
-      true: 'opacity-0',
-      false: 'opacity-100'
-    }
-  }
-});
 
 type Props = VariantProps<typeof buttonStyle> &
   ComponentProps<typeof AriaButton> & {
@@ -116,15 +42,91 @@ export const Button = forwardRef<HTMLButtonElement, Props>(function _Button(
     >
       {values => (
         <>
-          {iconBefore && <span>{iconBefore({})}</span>}
-          <span className={childrenSpanStyle({ isLoading })}>{getAriaRenderChildren(values, children)}</span>
-          {isLoading && (
-            <span className='absolute size-full flex items-center justify-center'>
-              <SpinLoader size='small' color='primary-text' />
+          {(iconBefore || isLoading) && (
+            <span className='flex items-center justify-center'>
+              {isLoading && <SpinLoader size='small' />}
+              {iconBefore && !isLoading && <Icon className='size-4' icon={iconBefore} />}
             </span>
           )}
+          <span className=''>{getAriaRenderChildren(values, children)}</span>
         </>
       )}
     </AriaButton>
   );
+});
+
+const buttonStyle = tv({
+  base: 'flex items-center font-medium justify-center transition-colors duration-300',
+  variants: {
+    size: {
+      small: 'gap-2 h-8 px-2 text-xs rounded',
+      default: 'gap-2 h-10 px-4 text-sm rounded-md',
+      large: 'gap-3 h-12 px-8 text-lg rounded-lg'
+    },
+    fullWidth: {
+      true: 'w-full',
+      false: ''
+    },
+    variant: {
+      ghost: '',
+      outline: 'border',
+      primary: ''
+    },
+    isLoading: {
+      true: 'relative',
+      false: ''
+    },
+    isDisabled: {
+      true: '',
+      false: ''
+    }
+  },
+  compoundVariants: [
+    {
+      variant: 'primary',
+      size: 'default',
+      className: 'font-semibold'
+    },
+    {
+      variant: 'primary',
+      size: 'large',
+      className: 'font-bold'
+    },
+    {
+      variant: 'primary',
+      isDisabled: true,
+      className: 'bg-primary-disabled text-primary-text'
+    },
+    {
+      variant: 'primary',
+      isDisabled: false,
+      isLoading: true,
+      className: 'bg-primary text-primary-text'
+    },
+    {
+      variant: 'primary',
+      isDisabled: false,
+      isLoading: false,
+      className:
+        'bg-primary text-primary-text hover:bg-primary-hover active:bg-primary-active focus:outline-offset-4 focus:outline-secondary-focus'
+    },
+    {
+      variant: 'outline',
+      isDisabled: true,
+      className: 'bg-background-white-disabled text-foreground-lowPriority border-edge-disabled'
+    },
+    {
+      variant: 'outline',
+      isDisabled: false,
+      isLoading: false,
+      className:
+        'bg-background-white border text-foreground-dark border-edge-default hover:bg-background-white-hover active:bg-background-white-active focus:outline-offset-4 focus:outline-secondary-focus'
+    },
+    {
+      variant: 'outline',
+      isDisabled: false,
+      isLoading: true,
+      className: 'bg-background-white border text-foreground-dark border-edge-default'
+    }
+  ]
 });

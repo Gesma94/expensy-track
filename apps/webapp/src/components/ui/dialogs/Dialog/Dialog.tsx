bc0d2b72dcf3c1a72b678e32b9b2caf0ba6af6c3
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2024-12-29 22:56:32 +0100

    feat(webapp): improve dialog animation

--- apps/webapp/src/components/ui/dialogs/Dialog/Dialog.tsx
+++ apps/webapp/src/components/ui/dialogs/Dialog/Dialog.tsx
@@ -20,14 +20,14 @@ export function Dialog({ children, dialogClassName, ...modalOverlayProps }: Prop
 }
 
 const ariaModalStyle = tv({
-  base: 'bg-white p-2',
+  base: 'bg-white rounded-lg',
   variants: {
     isEntering: {
-      true: 'animate-dialog-enter',
+      true: 'motion-scale-in-[0.5] motion-translate-x-in-[-40%] motion-translate-y-in-[-50%] motion-opacity-in-[0%] motion-rotate-in-[-10deg] motion-blur-in-[5px] motion-duration-[0.25s] motion-duration-[0.38s]/scale motion-duration-[0.37s]/translate motion-duration-[0.45s]/rotate motion-ease-spring-smooth',
       false: ''
     },
     isExiting: {
-      true: 'animate-dialog-exit',
+      true: 'motion-scale-out-[0.5] motion-translate-x-out-[40%] motion-translate-y-out-[-50%] motion-opacity-out-[0%] motion-rotate-out-[10deg] motion-blur-out-[5px] motion-duration-[0.25s] motion-duration-[0.38s]/scale motion-duration-[0.37s]/translate motion-duration-[0.45s]/rotate motion-ease-spring-smooth',
       false: ''
     }
   }

313972db808b063297f60ba913367269166f34a9
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-01-10 01:38:55 +0100

    fix(webapp): clean ColorPicker component

--- apps/webapp/src/components/ui/input/ColorPicker/ColorPicker.tsx
+++ apps/webapp/src/components/ui/input/ColorPicker/ColorPicker.tsx
@@ -1,41 +1,16 @@
 import type { ComponentProps, Ref } from 'react';
-import {
-  type ColorFormat as AriaColorFormat,
-  ColorPicker as AriaColorPicker,
-  type Color,
-  parseColor
-} from 'react-aria-components';
+import { ColorPicker as AriaColorPicker } from 'react-aria-components';
 import { ColorPickerPieces } from './ColorPickerPieces';
 
 type Props = ComponentProps<typeof AriaColorPicker> & {
-  colorFormat?: AriaColorFormat;
   className?: string;
   isDisabled?: boolean;
   ref?: Ref<HTMLInputElement>;
 };
 
-export function ColorPicker({ className, colorFormat, value, onChange, isDisabled, ref, ...props }: Props) {
-  function getValidValue(): string | Color | undefined {
-    const defaultValidValue = '#FFFFFF';
-
-    if (value === undefined) {
-      return undefined;
-    }
-
-    if (typeof value !== 'string') {
-      return value;
-    }
-
-    try {
-      parseColor(value);
-      return value;
-    } catch (error) {
-      return defaultValidValue;
-    }
-  }
-
+export function ColorPicker({ className, isDisabled, ref, ...props }: Props) {
   return (
-    <AriaColorPicker onChange={onChange} value={getValidValue()} {...props}>
+    <AriaColorPicker {...props}>
       <ColorPickerPieces isDisabled={isDisabled} className={className} ref={ref} />
     </AriaColorPicker>
   );
